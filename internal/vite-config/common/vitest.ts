7e939fd380d6caca5dc2f354c9269117453c9d9e
yugangping <yugangping@yugangping.com>
2024-06-04 02:06:28 +0800

    feat(vue): 新增vue的测试依赖

--- internal/vite-config/common/vitest.ts
+++ internal/vite-config/common/vitest.ts
@@ -6,19 +6,20 @@ export function setupViteTest(): InlineConfig {
 	return {
 		// 提供全局 API,同时在unplugin-auto-import中配置可自动导入，同时需要在命令上加入--globals
 		globals: true,
-		// 测试环境，模拟浏览器环境的库jsdom。默认值: 'node'
-		environment: 'jsdom',
+		// 测试环境，模拟浏览器环境的库happy-dom。默认值: 'node'
+		environment: 'happy-dom',
 		testTransformMode: {
 			web: [webRegex.source],
 		},
 		coverage: {
+			include: ['src'],
 			exclude: ['node_modules'],
 			//覆盖范围阈值选项
 			thresholds: {
 				branches: 80, //branches 分支覆盖率
-				functions: 100, //函数执行的覆盖率
+				functions: 80, //函数执行的覆盖率
 				lines: 80, //代码函数覆盖率
-				statements: -10, //声明的覆盖率
+				statements: 80, //声明的覆盖率
 			},
 		},
 	}
